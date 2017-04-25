a4b1b285964bd6b77588052842cafd6df48b440a
MitchelHdz <sebastianvicky_@hotmail.com>
2017-04-25 13:47:45 -0500

    Modal js script changed preventing scrolling when is open, #2

--- build/js/profile.js
+++ build/js/profile.js
@@ -6,20 +6,23 @@ var btn = document.getElementById("modalButton");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
+var body = document.getElementById('body');
 // When the user clicks on the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "block";
+	modal.classList.add('modal-open');
+	body.classList.add('modal-open');
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+	modal.classList.remove('modal-open');
+	body.classList.remove('modal-open');
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+	if (event.target == modal) {
+		modal.classList.remove('modal-open');
+		body.classList.remove('modal-open');
+	}
 }
