aea172cca3910e477ef11f1266482cf30ddca1a2
MitchelHdz <sebastianvicky_@hotmail.com>
2017-05-08 14:17:24 -0500

    #9 Adding modal script

--- build/js/scheduling.js
+++ build/js/scheduling.js
@@ -44,3 +44,32 @@ $.datepicker.regional['es'] = {
     yearSuffix: ''
   };
   $.datepicker.setDefaults($.datepicker.regional['es']);
+  // Get the modal
+var modal = document.getElementById('schedulingModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("saveDate");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+var body = document.getElementById('body');
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.classList.add('modal-open');
+  body.classList.add('modal-open');
+  google.maps.event.trigger(map, "resize");
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.classList.remove('modal-open');
+  body.classList.remove('modal-open');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.classList.remove('modal-open');
+    body.classList.remove('modal-open');
+  }
+}
