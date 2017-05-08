0ec9a7e3b6e41f3b14630922a5fa8d0decdccd43
MitchelHdz <sebastianvicky_@hotmail.com>
2017-05-08 12:52:48 -0500

    #13 Adding arrows script

--- build/js/dashboard.js
+++ build/js/dashboard.js
@@ -1,5 +1,7 @@
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 2,
-  slidesToScroll: 2
+  slidesToScroll: 2,
+  nextArrow: '<img src="./img/icons/flecha-carrusel.png" class="slider-next cursor-pointer">',
+  prevArrow: '<img src="./img/icons/flecha-carrusel-copy.png" class="slider-prev cursor-pointer">',
 });
