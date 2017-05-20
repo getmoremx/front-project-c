f7befa9a1665ba88cb8c6a7c6116e74c9d7f030b
enrique199413@gmail.com <enrique199413@gmail.com>
2017-05-20 17:17:02 -0500

    Update 2017-05-20T22:16:54.983Z

--- build/js/register.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('buyer').addEventListener('click', function () {
-	document.getElementById('seller').classList.remove('active');
-	document.getElementById('buyer').classList.add('active');
-});
-document.getElementById('seller').addEventListener('click', function () {
-	document.getElementById('buyer').classList.remove('active');
-	document.getElementById('seller').classList.add('active');
-});
